74aeb96bfd94da9e881d2cc371a1168fb0c47401
WangPan <wangpanxcx@163.com>
2018-05-27 00:11:56 +0800

    feign commit

---
+++ service-feign/src/main/java/com/hand/controller/HiController.java
@@ -0,0 +1,24 @@
+package com.hand.controller;
+
+import com.hand.service.SchedualServiceHi;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.RestController;
+
+/**
+ * @author WangPan
+ * @title HiController
+ * @description (描述此类的功能)
+ * @date: 2018/5/27 0:02
+ */
+@RestController
+public class HiController {
+    @Autowired
+    SchedualServiceHi schedualServiceHi;
+    @RequestMapping(value = "/hi",method = RequestMethod.GET)
+    public String sayHi(@RequestParam String name){
+        return schedualServiceHi.sayHiFromClientOne(name);
+    }
+}
---
+++ service-feign/src/main/java/com/hand/service/SchedualServiceHi.java
@@ -0,0 +1,18 @@
+package com.hand.service;
+
+import org.springframework.cloud.openfeign.FeignClient;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.RequestParam;
+
+/**
+ * @author WangPan
+ * @title SchedualServiceHi
+ * @description (描述此类的功能)
+ * @date: 2018/5/27 0:00
+ */
+@FeignClient(value = "service-hi")
+public interface SchedualServiceHi {
+    @RequestMapping(value = "/hi",method = RequestMethod.GET)
+    String sayHiFromClientOne(@RequestParam(value = "name") String name);
+}
