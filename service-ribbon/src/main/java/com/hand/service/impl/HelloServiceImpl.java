3d72376b7a3dd3630a581cd77d09fcaecf3bb3fc
WangPan <wangpanxcx@163.com>
2018-05-27 15:19:30 +0800

    fix

--- service-ribbon/src/main/java/com/hand/service/impl/HelloServiceImpl.java
+++ service-ribbon/src/main/java/com/hand/service/impl/HelloServiceImpl.java
@@ -1,6 +1,7 @@
 package com.hand.service.impl;
 
 import com.hand.service.HelloService;
+import com.netflix.hystrix.contrib.javanica.annotation.HystrixCommand;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 import org.springframework.web.client.RestTemplate;
@@ -17,7 +18,12 @@ public class HelloServiceImpl implements HelloService {
     RestTemplate restTemplate;
 
     @Override
+    @HystrixCommand(fallbackMethod = "hiError")
     public String hiService(String name) {
         return restTemplate.getForObject("http://SERVICE-HI/hi?name="+name,String.class);
+    }
+
+    public String hiError(String name) {
+        return "hi,"+name+",sorry,error!";
     }
 }
