f5c79ba16c9bd1a1c87bfce09385bb8b8ea711a4
WangPan <wangpanxcx@163.com>
2018-05-28 14:49:57 +0800

    fix commit

--- config-client/src/main/java/com/hand/configclient/ConfigClientApplication.java
+++ config-client/src/main/java/com/hand/configclient/ConfigClientApplication.java
@@ -2,8 +2,10 @@ package com.hand.configclient;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.ComponentScan;
 
 @SpringBootApplication
+@ComponentScan("com.hand")//扫描注入
 public class ConfigClientApplication {
 
     public static void main(String[] args) {
---
+++ config-client/src/main/java/com/hand/controller/HiController.java
@@ -0,0 +1,21 @@
+package com.hand.controller;
+
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+/**
+ * @author WangPan
+ * @title HiController
+ * @description (描述此类的功能)
+ * @date: 2018/5/28 14:41
+ */
+@RestController
+public class HiController {
+    @Value("${foo}")
+    String foo;
+    @RequestMapping(value = "/hi")
+    public String hi(){
+        return foo;
+    }
+}
